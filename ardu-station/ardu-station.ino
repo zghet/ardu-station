52adf26b141d3bafc199ddb2747955ceecb741dd
Zuriel <zghet@me.com>
2019-08-09 11:51:06 -0500

    Preparacion para comunicacion API, se eliminan varios cambios para reducir espacio en memoria

--- ardu-station/ardu-station.ino
+++ ardu-station/ardu-station.ino
@@ -45,19 +45,30 @@ EthernetServer server(81);
 // Inicializamos el sensor DHT11
 DHT dht(DHTPIN, DHTTYPE);
 
-// Inicializamos el sensor DHT11
-DHT dht(DHTPIN, DHTTYPE);
-
 //Ports
 #define LISTEN_PORT 81
+
+//rest
+aREST rest = aREST();
+
+//definimos variables para la api
+float humidity, temperature;
 
 ////////////////////////////////////////////////////////////////////////////
 
 void setup()
 {
-  //Inicializamos aREST
-  aRest rest = aREST();
 
+  //API
+  rest.variable("Temperatura:", &temperature);
+  rest.variable("Humedad:", &humidity);
+  
+  //identificacion
+  rest.set_id("1");
+  rest.set_name("Sensor NanoMCUv3");
+
+  //////////////////////////////////////////////////////////////////////
+  
   // Inicializamos comunicación serie
   Serial.begin(9600);
      
@@ -75,6 +86,7 @@ void setup()
   //inicializar servidor web
   server.begin();
   Serial.println("Server inicializado");
+  Serial.println("IP:" &IPAddress);
 
 
 }
@@ -83,60 +95,6 @@ void setup()
 
 void loop() 
 {
-    /////////////Se establece el html del server
-  EthernetClient client = server.available();  // Buscamos entrada de clientes
-  if (client) 
-  { 
-    Serial.println("new client");
-    boolean currentLineIsBlank = true;  // Las peticiones HTTP finalizan con linea en blanco
-    while (client.connected())
-    {
-      if (client.available())
-      {  
-        char c = client.read();
-        Serial.write(c);   // Esto no es necesario, pero copiamos todo a la consola
-        // A partir de aquí mandamos nuestra respuesta
-        if (c == '\n' && currentLineIsBlank) 
-        {   
-          // Enviar una respuesta tipica
-          client.println("HTTP/1.1 200 OK");             
-          client.println("Content-Type: text/html");
-          client.println("Connection: close");
-          client.println("Refresh: 15");            // Actualizar cada 15 segs
-          client.println();
-          client.println("<!DOCTYPE HTML>");
-          client.println("<html>");
-          float h = dht.readHumidity();           // Leer el sensor
-          float t = dht.readTemperature();
-          Serial.println(t);
-          Serial.println(h);
-          // Desde aqui creamos nuestra pagina con el codigo HTML que pongamos
-          client.print("<head><title>Situacion del lugar</title></head>");
-          client.print("<body><h1> Situacion Ambiente</h1><p>Temperatura -");
-          client.print(t);     // Aqui va la temperatura
-          client.print(" grados Celsius</p>");
-          client.print("<p>Humedad:  ");
-          client.print(h);    // Aqui va la humedad
-          client.print(" porciento</p>");
-          client.print("<p><em> La página se actualiza cada 15 segundos.</em></p></body></html>");
-          break;
-        }
-        if (c == '\n')
-        {
-          currentLineIsBlank = true;          // nueva linea
-        }
-        else if (c != '\r')
-        {
-          currentLineIsBlank = false;
-        }
-      }
-    }
-    delay(10);         // Para asegurarnos de que los datos se envia
-    client.stop();     // Cerramos la conexion
-    Serial.println("client disonnected");
-  }
-
- ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
   // Esperamos 4 segundos entre medidas
   delay(500);
@@ -156,23 +114,8 @@ void loop()
     // Calcular el índice de calor en grados centígrados
   float hic = dht.computeHeatIndex(t, h, false);
 
-  //Se deja la impresion en buffer por si se ocupa
-  //Impresion Serial
-  //Serial.print("Humedad: ");
-  //Serial.print(h);
-  //Serial.print(" % ");
-  //Serial.print("Temperatura: ");
-  //Serial.print(t);
-  //Serial.print(" *C\n ");
-
-
+ 
   /////////////////////////////////////////////////
-  //desplazamos una posición a la izquierda
-  lcd.scrollDisplayLeft(); 
-  delay(500);
-  
-  // Actualizar la pantalla completa cada segundo
-  //lcd.clear();
   
   //Impresion de temp
   lcd.setCursor(0,0);
@@ -186,9 +129,4 @@ void loop()
   lcd.setCursor(8,1);
   lcd.print(h);
 
-  //Impresion de ip
-  lcd.setCursor(14,0);
-  lcd.print("IP: ");
-  lcd.setCursor(17,0);
-  lcd.print("192.168.15.20"); 
 }
