6a321f5ecebfd23b3325115f8025f38aa36d13f9
Zuriel <zghet@me.com>
2019-08-09 09:37:26 -0500

    Se agreron los valores para cambiar el LCD de 16 pines por un por conexion SDACL

--- ardu-station/ardu-station.ino
+++ ardu-station/ardu-station.ino
@@ -6,24 +6,25 @@
 #include <Ethernet.h>
 
 //Libreria LCD
-#include <LiquidCrystal.h>
-LiquidCrystal lcd(4, 8, 9, 10, 11 , 12);
+#include <LiquidCrystal_I2C.h>
+//Se declara el tipo de LCD a usar
+LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 
 ////////////////////////////////////////////////////////////////////////////
  
-// the media access control (ethernet hardware) address for the shield:
+// Declaracion de mac address para shield ethernet:
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED }; 
 
-// the dns server ip
+// DNS Server
 IPAddress dnServer(192, 168, 15, 1);
 
-// the router gateway address:
+// Gateway:
 IPAddress gateway(192, 168, 15, 1);
 
-// the subnet:
+// Subnet:
 IPAddress subnet(255, 255, 255, 0);
 
-//the IP address is dependent on your network
+//ip Direction 
 IPAddress ip(192, 168, 15, 20);
 
 ////////////////////////////////////////////////////////////////////////////
@@ -46,10 +47,10 @@ void setup()
   // Comenzamos el sensor DHT
   dht.begin();
   
-  // MOVER EL CURSOR A LA PRIMERA POSICION DE LA PANTALLA (0, 0)
   //Se inicializa LCD
-  lcd.begin(16,2);
-  lcd.home();
+  lcd.begin(16, 2);
+  // Mover el cursor a la primera posición de la pantalla (0, 0)
+  lcd.clear();
 
   // initialize the ethernet device
   Ethernet.begin(mac, ip, dnServer, gateway, subnet);
@@ -59,74 +60,54 @@ void setup()
 ////////////////////////////////////////////////////////////////////////////
 
 void loop() 
-{
-  // EN EL CICLO PRINCIPAL SOLAMENTE RECORREMOS EL MENSAJE DE UN LADO A OTRO
-  // VARIABLE PARA CONTROL DE CICLOS
-  int i;
+// Esperamos 4 segundos entre medidas
+  delay(500);
 
-  // Esperamos 4 segundos entre medidas
-  delay(4000);
-
-  // DESPLAZAR LA PANTALLA A LA DERECHA 2 VECES
-  for ( int i = 0; i < 5; i++ ) 
-  {
-    lcd.scrollDisplayRight();
-    delay (1000);
-  }
-
-  // DESPLAZAR LA PANTALLA A LA IZQUIERDA 2 VECES
-  for ( int i = 0; i < 5; i++ ) 
-  {
-    lcd.scrollDisplayLeft();
-    delay (1000);
-  }
-
-  // Leemos la humedad relativa
+ // Leemos la humedad relativa
   float h = dht.readHumidity();
-
   // Leemos la temperatura en grados centígrados (por defecto)
   float t = dht.readTemperature();
-     
+   
   // Comprobamos si ha habido algún error en la lectura
   if (isnan(h) || isnan(t))
   {
-    // || isnan(f)) 
     Serial.println("Error obteniendo los datos del sensor DHT11");
     return;
   }
-
-  // Calcular el índice de calor en grados centígrados
-  float hic = dht.computeHeatIndex(t, h, false);
  
+    // Calcular el índice de calor en grados centígrados
+  float hic = dht.computeHeatIndex(t, h, false);
+
+ /////////////////////////////////////////////////
+ //Impresion Serial
   Serial.print("Humedad: ");
   Serial.print(h);
   Serial.print(" % ");
   Serial.print("Temperatura: ");
   Serial.print(t);
-  Serial.print(" *C\n");
+  Serial.print(" *C\n ");
 
-  //Impresion de temp y humedad  
-  delay(4000);
+
+/////////////////////////////////////////////////
+  //desplazamos una posición a la izquierda
+  lcd.scrollDisplayLeft(); 
+  delay(500);
+  // Actualizar la pantalla completa cada segundo
+  //lcd.clear();
+  //Impresion de temp
+  //delay(4000);
   lcd.setCursor(0,0);
   lcd.write("Temp: ");
   lcd.setCursor(5,0);
   lcd.print(t);
-
+  //Impresion de  humedad 
   lcd.setCursor(0,1);
   lcd.print("Humedad: ");
   lcd.setCursor(8,1);
   lcd.print(h);
-
-  // Limpiamos la pantalla
-  //lcd.clear();
-  
-  //print out the IP address
-  //delay(4000);
-  //lcd.setCursor(9,1);
-  //lcd.print("IP = ");
-  //lcd.print(Ethernet.localIP());
-  
-  // Limpiamos la pantalla
-  //lcd.clear();
- 
+  //Impresion de ip
+  lcd.setCursor(14,0);
+  lcd.print("IP: ");
+  lcd.setCursor(17,0);
+  lcd.print("192.168.15.20"); 
 }
