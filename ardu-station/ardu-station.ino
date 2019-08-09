376d8160ae7216bb2b15240b38dee0b581028d3e
Zuriel <zghet@me.com>
2019-08-09 10:26:10 -0500

    Se arreglo un conflicto en los archivos guarad: Se agregaron los detalles para la configuracion web y consulta en arduino de los datos obtenidos

--- ardu-station/ardu-station.ino
+++ ardu-station/ardu-station.ino
@@ -1,12 +1,16 @@
 // Incluimos librería de sensor de temperatura y humedad
 #include <DHT.h>
 
+//libreria APIRest
+#include <aREST.h>
+
 //libreria Ethernet
 #include <SPI.h>
 #include <Ethernet.h>
 
-//Libreria LCD
+//Libreria LCDI2C
 #include <LiquidCrystal_I2C.h>
+#include <Wire.h>
 //Se declara el tipo de LCD a usar
 LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 
@@ -27,7 +31,11 @@ IPAddress subnet(255, 255, 255, 0);
 //ip Direction 
 IPAddress ip(192, 168, 15, 20);
 
-////////////////////////////////////////////////////////////////////////////
+//InicializarCrear la instancia del server
+EthernetServer server(81);
+
+
+//////////////////////statement of variables and ports///////////////////////////////////
 
 // Definimos el pin digital donde se conecta el sensor
 #define DHTPIN 2
@@ -36,15 +44,24 @@ IPAddress ip(192, 168, 15, 20);
 
 // Inicializamos el sensor DHT11
 DHT dht(DHTPIN, DHTTYPE);
- 
- ////////////////////////////////////////////////////////////////////////////
+
+// Inicializamos el sensor DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
+//Ports
+#define LISTEN_PORT 81
+
+////////////////////////////////////////////////////////////////////////////
 
 void setup()
 {
+  //Inicializamos aREST
+  aRest rest = aREST();
+
   // Inicializamos comunicación serie
   Serial.begin(9600);
- 
-  // Comenzamos el sensor DHT
+     
+ // Comenzamos el sensor DHT
   dht.begin();
   
   //Se inicializa LCD
@@ -55,13 +72,73 @@ void setup()
   // initialize the ethernet device
   Ethernet.begin(mac, ip, dnServer, gateway, subnet);
 
+  //inicializar servidor web
+  server.begin();
+  Serial.println("Server inicializado");
+
+
 }
 
 ////////////////////////////////////////////////////////////////////////////
 
 void loop() 
 {
-// Esperamos 4 segundos entre medidas
+    /////////////Se establece el html del server
+  EthernetClient client = server.available();  // Buscamos entrada de clientes
+  if (client) 
+  { 
+    Serial.println("new client");
+    boolean currentLineIsBlank = true;  // Las peticiones HTTP finalizan con linea en blanco
+    while (client.connected())
+    {
+      if (client.available())
+      {  
+        char c = client.read();
+        Serial.write(c);   // Esto no es necesario, pero copiamos todo a la consola
+        // A partir de aquí mandamos nuestra respuesta
+        if (c == '\n' && currentLineIsBlank) 
+        {   
+          // Enviar una respuesta tipica
+          client.println("HTTP/1.1 200 OK");             
+          client.println("Content-Type: text/html");
+          client.println("Connection: close");
+          client.println("Refresh: 15");            // Actualizar cada 15 segs
+          client.println();
+          client.println("<!DOCTYPE HTML>");
+          client.println("<html>");
+          float h = dht.readHumidity();           // Leer el sensor
+          float t = dht.readTemperature();
+          Serial.println(t);
+          Serial.println(h);
+          // Desde aqui creamos nuestra pagina con el codigo HTML que pongamos
+          client.print("<head><title>Situacion del lugar</title></head>");
+          client.print("<body><h1> Situacion Ambiente</h1><p>Temperatura -");
+          client.print(t);     // Aqui va la temperatura
+          client.print(" grados Celsius</p>");
+          client.print("<p>Humedad:  ");
+          client.print(h);    // Aqui va la humedad
+          client.print(" porciento</p>");
+          client.print("<p><em> La página se actualiza cada 15 segundos.</em></p></body></html>");
+          break;
+        }
+        if (c == '\n')
+        {
+          currentLineIsBlank = true;          // nueva linea
+        }
+        else if (c != '\r')
+        {
+          currentLineIsBlank = false;
+        }
+      }
+    }
+    delay(10);         // Para asegurarnos de que los datos se envia
+    client.stop();     // Cerramos la conexion
+    Serial.println("client disonnected");
+  }
+
+ ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  // Esperamos 4 segundos entre medidas
   delay(500);
 
  // Leemos la humedad relativa
@@ -79,33 +156,36 @@ void loop()
     // Calcular el índice de calor en grados centígrados
   float hic = dht.computeHeatIndex(t, h, false);
 
- /////////////////////////////////////////////////
- //Impresion Serial
-  Serial.print("Humedad: ");
-  Serial.print(h);
-  Serial.print(" % ");
-  Serial.print("Temperatura: ");
-  Serial.print(t);
-  Serial.print(" *C\n ");
+  //Se deja la impresion en buffer por si se ocupa
+  //Impresion Serial
+  //Serial.print("Humedad: ");
+  //Serial.print(h);
+  //Serial.print(" % ");
+  //Serial.print("Temperatura: ");
+  //Serial.print(t);
+  //Serial.print(" *C\n ");
 
 
-/////////////////////////////////////////////////
+  /////////////////////////////////////////////////
   //desplazamos una posición a la izquierda
   lcd.scrollDisplayLeft(); 
   delay(500);
+  
   // Actualizar la pantalla completa cada segundo
   //lcd.clear();
+  
   //Impresion de temp
-  //delay(4000);
   lcd.setCursor(0,0);
   lcd.write("Temp: ");
   lcd.setCursor(5,0);
   lcd.print(t);
+  
   //Impresion de  humedad 
   lcd.setCursor(0,1);
   lcd.print("Humedad: ");
   lcd.setCursor(8,1);
   lcd.print(h);
+
   //Impresion de ip
   lcd.setCursor(14,0);
   lcd.print("IP: ");
