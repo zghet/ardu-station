e8e558b6429b5bb7b5d973a6aa43d30462b95d5e
Zuriel <zghet@me.com>
2019-11-22 15:55:35 -0600

    Agregamos definicion de relays

--- esp8266-alx/esp8266-alx.ino
+++ esp8266-alx/esp8266-alx.ino
@@ -6,10 +6,22 @@
 #include "config.h"
 #include "ESP8266-Util.hpp"
 
+// Empezamos a declarar variables
+#define RELAY_PIN_1 4
+#define RELAY_PIN_2 14
+#define LAMPARA1 "lampara uno"
+#define LAMPARA2 "lampara dos"
+
 void setup() 
 {
   Serial.begin(115200);
   ConnectWiFi_STA();
+
+  pinMode(RELAY_PIN_1, OUTPUT);
+  digitalWrite(RELAY_PIN_1, HIGH);
+
+  pinMode(RELAY_PIN_2, OUTPUT);
+  digitalWrite(RELAY_PIN_2, HIGH);
 }
 
 void loop() 
