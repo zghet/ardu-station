1342d25e6967e1f17b960bd67a4e00afc2326f21
Zuriel <zghet@me.com>
2019-12-05 19:09:45 -0600

    se cargan relays

--- esp8266-alx/esp8266-alx.ino
+++ esp8266-alx/esp8266-alx.ino
@@ -1,5 +1,3 @@
-
-
 #include <ESP8266WiFi.h>
 #include "fauxmoESP.h" 
 
@@ -15,32 +13,60 @@
 #define relay_for D3
 #define relay_fiv D4
 #define relay_six D5
-#define relay_one D6
-
+#define relay_sev D6
+#define relay_eig D7
+#define relay_nin D8
 
 #define serial_rate 115200 
 
-#define LAMPARA1 "luz comedor"
-#define LAMPARA2 "luz comida"
-#define LAMPARAS "luces"
+#define light1 "luz 1"
+#define light2 "luz 2"
+#define light3 "luz 3"
+#define light4 "luz 4"
+#define light5 "luz 5"
+#define light6 "luz 6"
+#define light7 "luz 7"
+#define light8 "luz 8"
+#define light9 "luz 9"
 
 fauxmoESP fauxmo;
 
 void setup() {
-  // Init serial port and clean garbage
+  // Init serial port 
   Serial.begin(serial_rate);
   Serial.println();
 
   // Wi-Fi connection
   wifiSetup();
 
-  // LED
-  pinMode(RELAY_PIN_1, OUTPUT);
-  digitalWrite(RELAY_PIN_1, HIGH);
-  //delay(500);
-  pinMode(RELAY_PIN_2, OUTPUT);
-  digitalWrite(RELAY_PIN_2,HIGH);
-  //delay(500);
+  // LED define
+  pinMode(relay_one, OUTPUT);
+  digitalWrite(relay_one, HIGH);
+
+  pinMode(relay_two, OUTPUT);
+  digitalWrite(relay_two,HIGH);
+
+  pinMode(relay_thr, OUTPUT);
+  digitalWrite(relay_thr, HIGH);
+
+  pinMode(relay_for, OUTPUT);
+  digitalWrite(relay_for,HIGH);
+
+  pinMode(relay_fiv, OUTPUT);
+  digitalWrite(relay_fiv, HIGH);
+
+  pinMode(relay_six, OUTPUT);
+  digitalWrite(relay_six,HIGH);
+
+  pinMode(relay_sev, OUTPUT);
+  digitalWrite(relay_sev, HIGH);
+
+  pinMode(relay_eig, OUTPUT);
+  digitalWrite(relay_eig,HIGH);
+
+  pinMode(relay_nin, OUTPUT);
+  digitalWrite(relay_nin,HIGH);
+
 
   // By default, fauxmoESP creates it's own webserver on the defined port
   // The TCP port must be 80 for gen3 devices (default is 1901)
@@ -56,58 +82,159 @@ void setup() {
   // "Alexa, turn lamp two on"
 
   // Add virtual devices
-  fauxmo.addDevice(LAMPARA1);
-  fauxmo.addDevice(LAMPARA2);
-  fauxmo.addDevice(LAMPARAS);
-    fauxmo.onSetState
+  fauxmo.addDevice(light1);
+  fauxmo.addDevice(light2);
+  fauxmo.addDevice(light3);
+  fauxmo.addDevice(light4);
+  fauxmo.addDevice(light5);
+  fauxmo.addDevice(light6);
+  fauxmo.addDevice(light7);
+  fauxmo.addDevice(light8);
+  fauxmo.addDevice(light9);
+  fauxmo.addDevice(light10);
+  fauxmo.onSetState
   (
     [](unsigned char device_id, const char * device_name, bool state, unsigned char value) 
   {
     Serial.printf("[MAIN] Device #%d (%s) state: %s value: %d\n", device_id, device_name, state ? "ON" : "OFF", value);
 
-  if ( (strcmp(device_name, "luz comedor") == 0) )
-  {
-  if (!state)
-  {
-  digitalWrite(RELAY_PIN_1, HIGH);
-  }
-  else
-  {
-  digitalWrite(RELAY_PIN_1, LOW);
-  }
-  }
-
-    if ( (strcmp(device_name, "luz comida") == 0) )
-  {
-  if (!state)
-  {
-  digitalWrite(RELAY_PIN_2, HIGH);
-  }
-  else
-  {
-  digitalWrite(RELAY_PIN_2, LOW);
-  }
-  }
-
-    if ( (strcmp(device_name, "luces") == 0) ) 
-  {
-    if (!state) 
+    if ( (strcmp(device_name, "light1") == 0) )
     {
-      digitalWrite(RELAY_PIN_1, HIGH);
-      digitalWrite(RELAY_PIN_2, HIGH);
-      //digitalWrite(RELAY_3, HIGH);
-      //digitalWrite(RELAY_4, HIGH);
-    } 
-    else 
+      if (!state)
+      {
+        digitalWrite(relay_one, HIGH);
+      }
+      else
+      {
+        digitalWrite(relay_one, LOW);
+      }
+    }
+
+    if ( (strcmp(device_name, "light2") == 0) )
     {
-      digitalWrite(RELAY_PIN_1, LOW);
-      digitalWrite(RELAY_PIN_2, LOW);
-      //digitalWrite(RELAY_3, LOW);
-      //digitalWrite(RELAY_4, LOW);
+      if (!state)
+      {
+        digitalWrite(relay_two, HIGH);
+      }
+      else
+      {
+        digitalWrite(relay_two, LOW);
+      }
+    }
+
+    if ( (strcmp(device_name, "light3") == 0) )
+    {
+      if (!state)
+      {
+        digitalWrite(relay_thr, HIGH);
+      }
+      else
+      {
+        digitalWrite(relay_thr, LOW);
+      }
+    }
+
+    if ( (strcmp(device_name, "light4") == 0) )
+    {
+      if (!state)
+      {
+        digitalWrite(relay_for, HIGH);
+      }
+      else
+      {
+        digitalWrite(relay_for, LOW);
+      }
+    }
+
+    if ( (strcmp(device_name, "light5") == 0) )
+    {
+      if (!state)
+      {
+        digitalWrite(relay_fiv, HIGH);
+      }
+      else
+      {
+        digitalWrite(relay_fiv, LOW);
+      }
+    }
+
+    if ( (strcmp(device_name, "light6") == 0) )
+    {
+      if (!state)
+      {
+        digitalWrite(relay_six, HIGH);
+      }
+      else
+      {
+        digitalWrite(relay_six, LOW);
+      }
+    }
+
+    if ( (strcmp(device_name, "light7") == 0) )
+    {
+      if (!state)
+      {
+        digitalWrite(relay_sev, HIGH);
+      }
+      else
+      {
+        digitalWrite(relay_sev, LOW);
+      }
+    }
+
+    if ( (strcmp(device_name, "light8") == 0) )
+    {
+      if (!state)
+      {
+        digitalWrite(relay_eig, HIGH);
+      }
+      else
+      {
+        digitalWrite(relay_eig, LOW);
+      }
+    }
+
+    if ( (strcmp(device_name, "light9") == 0) )
+    {
+      if (!state)
+      {
+        digitalWrite(relay_nin, HIGH);
+      }
+      else
+      {
+        digitalWrite(relay_nin, LOW);
+      }
+    }
+
+    if ( (strcmp(device_name, "light10") == 0) ) 
+    {
+      if (!state) 
+      {
+        digitalWrite(relay_one, HIGH);
+        digitalWrite(relay_two, HIGH);
+        digitalWrite(relay_thr, HIGH);
+        digitalWrite(relay_for, HIGH);
+        digitalWrite(relay_fiv, HIGH);
+        digitalWrite(relay_six, HIGH);
+        digitalWrite(relay_sev, HIGH);
+        digitalWrite(relay_eig, HIGH);
+        digitalWrite(relay_nin, HIGH);
+      } 
+      else 
+      {
+        digitalWrite(relay_one, LOW);
+        digitalWrite(relay_two, LOW);
+        digitalWrite(relay_thr, LOW);
+        digitalWrite(relay_for, LOW);
+        digitalWrite(relay_fiv, LOW);
+        digitalWrite(relay_six, LOW);
+        digitalWrite(relay_sev, LOW);
+        digitalWrite(relay_eig, LOW);
+        digitalWrite(relay_nin, LOW);
+      }
     }
   }
-  });
-
+  );
 }
 
 void loop() 
