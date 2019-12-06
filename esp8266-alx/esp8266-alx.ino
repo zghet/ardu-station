5e0ac3aeccef77f8032c73b0dcb0c26cb159a087
Zuriel <zghet@me.com>
2019-12-05 18:10:08 -0600

    Cambio de branch agregando relays

--- esp8266-alx/esp8266-alx.ino
+++ esp8266-alx/esp8266-alx.ino
@@ -1,54 +1,116 @@
-// se incluyen librerias
+
+
 #include <ESP8266WiFi.h>
+#include "fauxmoESP.h" 
 
 //Se mandan a llamar los archivos de wifi
 #include "config.h"
 #include "ESP8266-Util.hpp"
 
 
-// Se definen los dipositivos
-#define relay_1 3
-#define relay_2 4
+// Se definen los pines
+#define relay_one D0
+#define relay_two D1
+#define relay_thr D2
+#define relay_for D3
+#define relay_fiv D4
+#define relay_six D5
+#define relay_one D6
 
 
-#include "fauxmoESP.h" 
-#define SERIAL_BAUDRATE 115200
+#define serial_rate 115200 
 
-#define lamp1 "luz julio"
-#define lamp2 "luz visita"
-
+#define LAMPARA1 "luz comedor"
+#define LAMPARA2 "luz comida"
+#define LAMPARAS "luces"
 
 fauxmoESP fauxmo;
 
-void setup() 
-{
-  
-  Serial.begin(SERIAL_BAUDRATE);
+void setup() {
+  // Init serial port and clean garbage
+  Serial.begin(serial_rate);
   Serial.println();
-  
+
+  // Wi-Fi connection
   wifiSetup();
 
-  //Define relays
-  pinMode(relay_1, OUTPUT);
-  digitalWrite(relay_1, HIGH);
+  // LED
+  pinMode(RELAY_PIN_1, OUTPUT);
+  digitalWrite(RELAY_PIN_1, HIGH);
+  //delay(500);
+  pinMode(RELAY_PIN_2, OUTPUT);
+  digitalWrite(RELAY_PIN_2,HIGH);
+  //delay(500);
 
-  pinMode(relay_2, OUTPUT);
-  digitalWrite(relay_2, HIGH);
-
-  // Add virtual devices
-  fauxmo.addDevice(lamp1);
-  fauxmo.addDevice(lamp2);
-  
   // By default, fauxmoESP creates it's own webserver on the defined port
+  // The TCP port must be 80 for gen3 devices (default is 1901)
+  // This has to be done before the call to enable()
   fauxmo.createServer(true); // not needed, this is the default value
   fauxmo.setPort(80); // This is required for gen3 devices
-  
+
   // You have to call enable(true) once you have a WiFi connection
+  // You can enable or disable the library at any moment
+  // Disabling it will prevent the devices from being discovered and switched
   fauxmo.enable(true);
+  // You can use different ways to invoke alexa to modify the devices state:
+  // "Alexa, turn lamp two on"
+
+  // Add virtual devices
+  fauxmo.addDevice(LAMPARA1);
+  fauxmo.addDevice(LAMPARA2);
+  fauxmo.addDevice(LAMPARAS);
+    fauxmo.onSetState
+  (
+    [](unsigned char device_id, const char * device_name, bool state, unsigned char value) 
+  {
+    Serial.printf("[MAIN] Device #%d (%s) state: %s value: %d\n", device_id, device_name, state ? "ON" : "OFF", value);
+
+  if ( (strcmp(device_name, "luz comedor") == 0) )
+  {
+  if (!state)
+  {
+  digitalWrite(RELAY_PIN_1, HIGH);
+  }
+  else
+  {
+  digitalWrite(RELAY_PIN_1, LOW);
+  }
+  }
+
+    if ( (strcmp(device_name, "luz comida") == 0) )
+  {
+  if (!state)
+  {
+  digitalWrite(RELAY_PIN_2, HIGH);
+  }
+  else
+  {
+  digitalWrite(RELAY_PIN_2, LOW);
+  }
+  }
+
+    if ( (strcmp(device_name, "luces") == 0) ) 
+  {
+    if (!state) 
+    {
+      digitalWrite(RELAY_PIN_1, HIGH);
+      digitalWrite(RELAY_PIN_2, HIGH);
+      //digitalWrite(RELAY_3, HIGH);
+      //digitalWrite(RELAY_4, HIGH);
+    } 
+    else 
+    {
+      digitalWrite(RELAY_PIN_1, LOW);
+      digitalWrite(RELAY_PIN_2, LOW);
+      //digitalWrite(RELAY_3, LOW);
+      //digitalWrite(RELAY_4, LOW);
+    }
+  }
+  });
 
 }
 
-void loop()
+void loop() 
 {
   // fauxmoESP uses an async TCP server but a sync UDP server
   // Therefore, we have to manually poll for UDP packets
